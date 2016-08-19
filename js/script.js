6d786a6d170a4518f1c5b731412dc909a4346c97
Sanny <sanny@SannydeMacBook.local>
2016-08-19 22:58:50 +0800

    some changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-window.onload = sayHello();
 
-function sayHello() {
-  alert("hello, there");
+window.onload=prepareLink;
+function popUp(winURL) {
+	window.open(winURL,"popup","width=320,height=480");
+}
+
+
+function prepareLink() {
+	var links=document.getElementsByTagName('a');
+	for (var i = 0; i < links.length; i++) {
+		if(links[i].getAttribute("class")=="popup"){
+			links[i].onclick=function() {
+				popUp(this.getAttribute("href"));
+				return false;
+			}
+		}	
+	}
 }
